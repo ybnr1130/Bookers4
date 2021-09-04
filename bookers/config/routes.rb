0e05f5ead63b0ee28d31169e1be26784b39c1141
ybnr1130 <muuckk@gmail.com>
2021-09-04 07:53:35 +0000

    [Add]relationshipモデルとコントローラー

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
+
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get 'relationships/create'
+  get 'relationships/destroy'
 
   devise_for :users
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+      get :follows, on: :member
+      get :followers, on: :member
+  end
+  
   resources :books, only: [:show, :index, :create, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  # View作成から
 
 end

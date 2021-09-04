35e8e5c069b322ac47c96468524e0b70c7189311
ybnr1130 <muuckk@gmail.com>
2021-09-04 15:05:15 +0000

    [Finish]

--- bookers/app/controllers/relationships_controller.rb
+++ bookers/app/controllers/relationships_controller.rb
@@ -10,11 +10,15 @@ class RelationshipsController < ApplicationController
     follow.destroy
     redirect_to request.referer
   end
-  
+
   def follows
+    user = User.find(params[:user_id])
+    @users = user.followings
   end
-  
+
   def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
   end
-  
+
 end

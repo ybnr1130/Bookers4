13b2693b6dd6fb23ae35e137c109d10a534e1582
ybnr1130 <muuckk@gmail.com>
2021-09-04 11:43:02 +0000

    [Add]follow機能追加

--- bookers/app/controllers/relationships_controller.rb
+++ bookers/app/controllers/relationships_controller.rb
@@ -1,7 +1,20 @@
 class RelationshipsController < ApplicationController
   def create
+    follow = Relationship.new(follower_id: params[:user_id], following_id: current_user.id)
+    follow.save
+    redirect_to request.referer
   end
 
   def destroy
+    follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    follow.destroy
+    redirect_to request.referer
   end
+  
+  def follows
+  end
+  
+  def followers
+  end
+  
 end
--- bookers/app/models/user.rb
+++ bookers/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   # ====================自分がフォローしているユーザーとの関連 ===================================
   #フォローする側のUserから見て、フォローされる側のUserを(中間テーブルを介して)集める。なので親はfollowing_id(フォローする側)
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
@@ -21,9 +21,15 @@ class User < ApplicationRecord
   # 中間テーブルを介して「following」モデルのUser(フォローする側)を集めることを「followers」と定義
   has_many :followers, through: :passive_relationships, source: :following
   # =======================================================================================
-  
+
+
   attachment :profile_image, destroy: false
 
   validates :name, length: { maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: { maximum: 50}
+
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
+
 end
